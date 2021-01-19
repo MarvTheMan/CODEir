4043326ed9a1a07b46a59e90dc981813b7af48c8
damiaanh <d.houtschild@gmail.com>
2021-01-20 00:36:07 +0100

    wrote func for stemming and lemmatizing

--- matrix.py
+++ matrix.py
@@ -9,12 +9,13 @@ def create_freq_matrix(wordcounts):
 def create_term_weight_matrix(freq_matrix):
     N = len(freq_matrix.columns)
     idf_list = []
-    for word, row in freq_matrix.iterrows():
+    for index, item in freq_matrix.iterrows():
         df = 0
         for text in freq_matrix.columns:
-            if row[text] != 0.0:
+            if item[text] != 0.0:
                 df+=1
         idf = log((N/df), 2)
         idf_list.append(idf)
     term_weight_matrix = freq_matrix.mul(idf_list, axis=0)
     return term_weight_matrix
+    
