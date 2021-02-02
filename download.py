e1a110a38a96f6cc1ce4bba75bba04253a9f96bf
damiaanh <d.houtschild@gmail.com>
2021-02-02 15:27:00 +0100

    changes loop for tw matrix

--- download.py
+++ download.py
@@ -2,7 +2,7 @@ import nltk
 import ssl
 
 """
-This file downloads the wordnet extention of nltk.
+This file downloads the wordnet extension of nltk.
 It can not be downloaded with pip.
 It looks complicated because the NLTK server can respond badly on a wrong ssl certificate,
 so this script disables the SSL check.
